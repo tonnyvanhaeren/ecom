3c80fdfd72453e9c7789d009f6d4a8da815d5ddb
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2018-04-06 17:33:12 +0200

    add categories shop product login pages

--- public/item.php
+++ public/item.php
@@ -28,7 +28,7 @@
     <div class="row">
 
         <div class="col-md-7">
-          <img class="img-responsive" src="http://placehold.it/700x600" alt="">
+          <img class="img-responsive" src="<?php echo $row['product_image']; ?>" alt="">
         </div>
 
         <div class="col-md-5">
@@ -49,7 +49,7 @@
                 </p>
               </div>
               
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <p><?php echo $row['short_desc']; ?></p>
       
             <form action="">
                 <div class="form-group">
--- resources/functions.php
+++ resources/functions.php
@@ -2,6 +2,25 @@
 
 //helper functions
 
+function set_message($msg){
+
+  if(!empty($msg)) {
+    $_SESSION['message'] = $msg;
+  }
+  else{
+    $msg = "";
+  }
+}
+
+function display_message() {
+
+  if(isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+  }
+
+}
+
 function redirect($location){
   header("Location: $location");
 }
@@ -63,6 +82,65 @@ echo $product;
   }
 }
 
+function get_category_products($cat_id){
+  $query = query(" SELECT * FROM products WHERE product_category_id=" . $cat_id . " ");
+
+  confirm($query);
+
+  while($row = fetch_array($query)){
+
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+<div class="thumbnail">
+    <img src="{$row['product_image']}" alt="">
+    <div class="caption">
+      <h3>{$row['product_title']}</h3>
+      <p>ult</p> 
+      <p>
+        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+      </p>
+    </div>
+</div>
+</div>
+
+DELIMETER;
+
+echo $product;
+
+  }
+}
+
+function get_products_in_shop_page(){
+
+  $query = query(" SELECT * FROM products");
+
+  confirm($query);
+
+  while($row = fetch_array($query)){
+
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+<div class="thumbnail">
+    <img src="{$row['product_image']}" alt="">
+    <div class="caption">
+      <h3>{$row['product_title']}</h3>
+      <p>ult</p> 
+      <p>
+        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+      </p>
+    </div>
+</div>
+</div>
+
+DELIMETER;
+
+echo $product;
+
+  }
+}
+
 // get categories
 function get_categories(){
 
@@ -77,6 +155,26 @@ DELIMETER;
 
   echo $category_link;  
 
+  }
+}
+
+//login function
+function login_user(){
+  if(isset($_POST['submit'])){
+    $username = escape_string($_POST['username']);
+    $password = escape_string($_POST['password']);
+
+    $query = query("SELECT * FROM users WHERE username ='{$username}' AND password ='{$password}' ");
+    confirm($query);
+
+    if(mysqli_num_rows($query) == 0 ) {
+      set_message("Your Password or Username is wrong!");
+      redirect("login.php");
+    }
+    else {
+      //set_message("Welcome to Admin {$username} ");
+      redirect("admin");
+    } 
   }
 }
 
